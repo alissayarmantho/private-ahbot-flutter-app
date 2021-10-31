c7df268d1c4800983867ecc240c35b77dcf8698d
Alissa <alissayarmantho1@gmail.com>
2021-10-31 19:21:28 +0800

    Add analytics controller and api

--- lib/controllers/analytics_controller.dart
+++ lib/controllers/analytics_controller.dart
@@ -1,1 +1,239 @@
+import 'package:botapp/models/calllog.dart';
+import 'package:botapp/models/contact.dart';
+import 'package:botapp/models/medialog.dart';
+import 'package:botapp/services/analytics.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+class AnalyticController extends GetxController {
+  var callLogs = List<CallLog>.from([]).obs;
+  var mediaLogs = List<MediaLog>.from([]).obs;
+  var filterName = "".obs;
+  var filteredContactList = List<Contact>.from([]).obs;
+  var isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  void fetchAllCallLogs({required String elderId}) async {
+    isLoading(true);
+
+    try {
+      await AnalyticService.fetchAllCallLogs(elderId: elderId).then((res) {
+        callLogs.value = res;
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting All Call Logs",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void fetchAllMediaLogs({required String elderId}) async {
+    isLoading(true);
+
+    try {
+      await AnalyticService.fetchAllMediaLogs(elderId: elderId).then((res) {
+        mediaLogs.value = res;
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting All Media Logs",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  Future<List<int>> getCallQuantity({required String elderId}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getCallQuantity(elderId: elderId).then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Call Quantity",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting call quantity");
+  }
+
+  Future<List<int>> getCallDuration({required String elderId}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getCallDuration(elderId: elderId).then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Call Duration",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting call duration");
+  }
+
+  Future<List<int>> getAppointmentStats({required String elderId}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getAppointmentStats(elderId: elderId).then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Appointment Statistics",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting appointment statistics");
+  }
+
+  Future<List<int>> getMedicationStats({required String elderId}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getMedicationStats(elderId: elderId).then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Medication Statistics",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting medication statistics");
+  }
+
+  Future<int> getMusicActivityDuration({required String elderId}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getMusicActivityDuration(elderId: elderId)
+          .then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Music Activity Duration",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting music activity duration");
+  }
+
+  Future<int> getMediaActivityDuration(
+      {required String elderId, required String mediaType}) async {
+    isLoading(true);
+    try {
+      await AnalyticService.getMediaActivityDuration(
+              elderId: elderId, mediaType: mediaType)
+          .then((res) {
+        return Future.value(res);
+      }).catchError((err) {
+        Get.snackbar(
+          "Error Getting Media Activity Duration",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+    // if it gets here it errors anyways I believe
+    throw Future.error("Error getting media activity duration");
+  }
+
+  void createCallLog({
+    required String callType,
+    required String startTime,
+    required String endTime,
+  }) async {
+    isLoading(true);
+    try {
+      await AnalyticService.createCallLog(
+        callType: callType,
+        startTime: startTime,
+        endTime: endTime,
+      ).catchError((err) {
+        Get.snackbar(
+          "Error Creating Call Log",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void createMediaLog({
+    required String mediaType,
+    required String startTime,
+    required String endTime,
+  }) async {
+    isLoading(true);
+    try {
+      await AnalyticService.createMediaLog(
+        mediaType: mediaType,
+        startTime: startTime,
+        endTime: endTime,
+      ).catchError((err) {
+        Get.snackbar(
+          "Error Creating Media Log",
+          err,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+          backgroundColor: Colors.red,
+        );
+      });
+    } finally {
+      isLoading(false);
+    }
+  }
+}
---
+++ lib/services/analytics.dart
@@ -0,0 +1,207 @@
+import 'dart:convert';
+
+import 'package:botapp/models/calllog.dart';
+import 'package:botapp/models/medialog.dart';
+import 'package:botapp/services/api_constants.dart';
+
+import 'base_api.dart';
+
+class AnalyticService {
+  static final String analyticUrl = "/api/analytic";
+
+  static Future<List<MediaLog>> fetchAllMediaLogs(
+      {required String elderId}) async {
+    String url = base_api + analyticUrl + "/all/medialog";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return List<MediaLog>.from(
+          jsonString['data']['mediaLogs'].map((x) => MediaLog.fromJson(x)),
+        );
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting all media logs error");
+    }
+  }
+
+  static Future<List<CallLog>> fetchAllCallLogs(
+      {required String elderId}) async {
+    String url = base_api + analyticUrl + "/all/calllog";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return List<CallLog>.from(
+          jsonString['data']['callLogs'].map((x) => CallLog.fromJson(x)),
+        );
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting all call logs error");
+    }
+  }
+
+  static Future<List<int>> getCallQuantity({required String elderId}) async {
+    String url = base_api + analyticUrl + "/call-quantity";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // index 0 for video call num and index 1 for voice call num
+        return [
+          jsonString['data']['videocallNum'],
+          jsonString['data']['voicecallNum']
+        ];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting call quantity error");
+    }
+  }
+
+  static Future<List<int>> getCallDuration({required String elderId}) async {
+    String url = base_api + analyticUrl + "/call-duration-stats";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // index 0 for video call num and index 1 for voice call num
+        return [
+          jsonString['data']['videocallDurationMinutes'],
+          jsonString['data']['voiceCallDurationMinutes']
+        ];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting call duration error");
+    }
+  }
+
+  static Future<List<int>> getAppointmentStats(
+      {required String elderId}) async {
+    String url = base_api + analyticUrl + "/appointment-stats";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // index 0 for completed appointments and index 1 for total appointments
+        return [
+          jsonString['data']['completedAppointment'],
+          jsonString['data']['totalAppointments']
+        ];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting appointment statistics error");
+    }
+  }
+
+  static Future<List<int>> getMedicationStats({required String elderId}) async {
+    String url = base_api + analyticUrl + "/medication-stats";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // index 0 for completed medications and index 1 for total medications
+        return [
+          jsonString['data']['completedMedication'],
+          jsonString['data']['totalMeds']
+        ];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting medication statistics error");
+    }
+  }
+
+  static Future<int> getMusicActivityDuration({required String elderId}) async {
+    String url = base_api + analyticUrl + "/music-activity-duration";
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return jsonString['data']['musicTotalMin'];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting music activity duration error");
+    }
+  }
+
+  // only media of type picture and video and probably will get cut out this one
+  static Future<int> getMediaActivityDuration(
+      {required String elderId, required String mediaType}) async {
+    String url = base_api +
+        analyticUrl +
+        "/media-activity-duration?mediaType=" +
+        mediaType;
+    try {
+      var response = await BaseApi.get(url: url);
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return jsonString['data']['mediaTotalMin'];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Getting media activity duration error");
+    }
+  }
+
+  static Future<String> createCallLog({
+    required callType,
+    required startTime,
+    required endTime,
+  }) async {
+    String url = base_api + analyticUrl + "/calllog";
+
+    try {
+      var response = await BaseApi.post(url: url, body: <String, String>{
+        'callType': callType,
+        'startTime': startTime,
+        'endTime': endTime,
+      });
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return jsonString['msg'];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Creating call log error");
+    }
+  }
+
+  static Future<String> createMediaLog({
+    required mediaType,
+    required startTime,
+    required endTime,
+  }) async {
+    String url = base_api + analyticUrl + "/medialog";
+
+    try {
+      var response = await BaseApi.post(url: url, body: <String, String>{
+        'mediaType': mediaType,
+        'startTime': startTime,
+        'endTime': endTime,
+      });
+      var jsonString = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return jsonString['msg'];
+      } else {
+        return Future.error(jsonString['msg']);
+      }
+    } catch (e) {
+      return Future.error("Creating media log error");
+    }
+  }
+}
